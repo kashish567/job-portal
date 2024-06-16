7fe4d1b426246faf38507c6b1e8d9c3490ccdacb
kashish567 <kashishbhanushali7@gmail.com>
2024-06-16 13:39:11 +0530

    navbar responsive

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -13,15 +14,15 @@ const Navbar = () => {
     { path: "/post-job", title: "Post a Job" },
   ];
   return (
-    <header>
-      <nav>
+    <header className="max-w-screen-2xl container mx-auto xl:px-24 px-4">
+      <nav className="flex justify-between items-center py-6">
         <a href="/" className="flex items-center gap-2 text-2xl">
           <img
             src="./images/Linear.png"
             alt="logo"
             className="size-8 gap-3 left-5 top-5"
           />
-          Job Portal
+          <span className="font-bold">JobPortal</span>
         </a>
         {/* nav for large devices */}
         <ul className="hidden md:flex gap-12">
@@ -39,14 +40,55 @@ const Navbar = () => {
 
         {/* Signup and login */}
         <div className="text-base text-primary font-medium space-x-5 hidden lg:block">
-          <Link to="/login" className="text-primary">
+          <Link to="/login" className="py-2 px-5 border rounded">
             Log in
           </Link>
-          <Link to="/signup" className="text-primary">
+          <Link
+            to="/signup"
+            className="py-2 px-5 border rounded bg-blue text-white"
+          >
             Sign up
           </Link>
         </div>
+        {/* mobile menu */}
+        <div className="md:hidden block">
+          <button onClick={handleMenuToggler}>
+            {isMenuOpen ? (
+              <FaTimes className="w-5 h-5 text-primary" />
+            ) : (
+              <FaBars className="w-5 h-5 text-primary" />
+            )}{" "}
+          </button>
+        </div>
       </nav>
+      {/* mobile nav */}
+      <div
+        className={`px-4 bg-black py-5 rounded-sm ${
+          isMenuOpen ? "" : "hidden"
+        }`}
+      >
+        <ul>
+          {navItems.map(({ path, title }) => (
+            <li
+              key={path}
+              className="text-base text-white first:text-white py-1"
+            >
+              <NavLink
+                to={path}
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                {title}
+              </NavLink>
+            </li>
+          ))}
+
+          <li className="text-white py-1">
+            <Link to="/login" className="py-2 rounded">
+              Log in
+            </Link>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 };
