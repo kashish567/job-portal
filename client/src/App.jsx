5d3f6b23509c3c94f74ede30b1078abc80a108a3
kashish567 <kashishbhanushali7@gmail.com>
2024-06-16 11:02:21 +0530

    app.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,13 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import { Outlet } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <nav>Navbar</nav>
+      <Outlet />
+    </>
+  );
 }
+
+export default App;
--- client/src/Router/Router.jsx
+++ client/src/Router/Router.jsx
@@ -1,10 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App.jsx";
+import Home from "../Pages/Home.jsx";
+import About from "../Pages/About.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
   },
 ]);
 
